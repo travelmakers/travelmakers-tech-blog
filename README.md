b43e6610991bef91ba95999f8236c72ffde09da7
Seong Gi Dong <sgd0947@gmail.com>
2022-08-24 10:35:27 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# How to run
+
+1. `npm i`
+2. `npm run dev`
+3. Server will be running on `localhost:3000`
+
+# Notes
+
+- This project used [Tailwind Next.js starter blog template](https://github.com/timlrx/tailwind-nextjs-starter-blog).
+- I'm using Node.js 16.xx.
---
+++ data/projectsData.js
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'My Personal Website',
+    description: `I'm developing my personal website with Next.js and Tailwind CSS. If you want to see its code, check my GitHub account.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://mreza.vercel.app',
+  },
+]
+
+export default projectsData
